c7a5513ea36137643282892126a902879ca5469c
Akira <am.1.00@bk.ru>
2022-07-17 10:42:10 +0300

    addicinal #3

--- src/losted1/accounts/views.py
+++ src/losted1/accounts/views.py
@@ -30,8 +30,6 @@ class LoginView(APIView):
 class LogoutView(APIView):
 
     def get(self, request):
-        if not request.user.is_authenticated:
-            return redirect('/login/')
         logout(request=request)
         return redirect('/login/')
 
